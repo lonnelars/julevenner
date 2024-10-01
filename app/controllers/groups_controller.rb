80d9de8590f9aa6d509cd8c80b94398db3f4e10c
Lars Lønne <lars.lonne@kantega.no>
2024-10-01 21:10:46 +0200

    working, but missing styling

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,5 +3,16 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @julevenn = find_julevenn session[:userinfo]["name"]
+  end
+
+  private
+
+  def find_julevenn(email)
+    ids = @group.users.all.map { |u| u.id }
+    alist = ids.zip(ids.shuffle(random: Random.new(@group.seed)))
+    me = User.find_by email: email
+    pair = alist.assoc me.id
+    User.find pair[1]
   end
 end
